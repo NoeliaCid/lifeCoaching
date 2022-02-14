286d5cbdaf6a4bc87bfbe15162c6b817c830a57b
Noelia Cid <noelia.cid.developer@gmail.com>
2022-02-13 22:30:57 -0300

    add: implementamos formulario

--- js/scripts.js
+++ js/scripts.js
@@ -52,3 +52,27 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
 });
+
+
+
+//FUNCIONALIDAD PARA EL FORMULARIO DE CONTACTO
+
+const $form = document.querySelector('#form')
+
+$form.addEventListener('submit', handleSubmit)
+
+async function handleSubmit(event) {
+    event.preventDefault()
+    const form = new FormData(this)
+    const response = await fetch(this.action, {
+        method: this.method,
+        body:form,
+        headers: {
+            'Accept': 'application/json'
+        }
+    })
+    if (response.ok){
+        this.reset()
+        alert('se envió el mensaje!')
+    }
+}
